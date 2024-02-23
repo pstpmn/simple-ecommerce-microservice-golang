0ecf04c6f597ebbccb3344c2d099ea5ddc4e859e
Kang <63038044+pstpmn@users.noreply.github.com>
2024-02-23 15:57:58 +0700

    add func convert string to objectId mongo

--- pkg/helper.go
+++ pkg/helper.go
@@ -1,13 +1,28 @@
 package pkg
 
 import (
+	"encoding/hex"
+	"fmt"
+
 	"github.com/google/uuid"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type IHelper interface {
+	ConvertStrToPrimitiveObjectId(value string, target *primitive.ObjectID) error
 	GenUuid() string
 }
 type helper struct {
+}
+
+// ConvertStrToPrimitiveObjectId implements IHelper.
+func (*helper) ConvertStrToPrimitiveObjectId(value string, target *primitive.ObjectID) error {
+	_, err := hex.DecodeString(value)
+	if err != nil {
+		return fmt.Errorf("Invalid hexadecimal string: %v", err)
+	}
+	*target, err = primitive.ObjectIDFromHex(value)
+	return err
 }
 
 // GenUuid implements IHelper.
